ce0c3bea55543268474ed259af4cffece776e67b
mactkg <mactkg@gmail.com>
2014-06-25 00:29:28 +0900

    cleaning

--- apitrace.rb
+++ apitrace.rb
@@ -1,7 +1,5 @@
 require "formula"
 
-#                /usr/local/Library/Contributions/example-formula.rb
-
 class Apitrace < Formula
   homepage "http://apitrace.github.io"
   url "https://github.com/apitrace/apitrace/archive/5.0.tar.gz"
@@ -12,20 +10,17 @@ class Apitrace < Formula
   end
 
   option "with-gui", "Build with gui support"
-  
   depends_on "qt" if build.with? "gui"
   depends_on "qjson" if build.with? "gui"
+  
   depends_on "cmake" => :build
   depends_on "python" => :build
 
   def install
     args = std_cmake_args
     args << "-DENABLE_GUI=TRUE" if build.with? "gui"
+ 
     system "cmake", ".", *args
     system "make", "install"
-  end
-
-  test do
-    system "false"
   end
 end
